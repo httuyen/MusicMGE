3db73a1b77a4e7a7d20c595edf5f262d98e59a51
Tuyen Huynh <47501532+httuyen@users.noreply.github.com>
2019-08-27 18:16:36 +0700

    August 27 2019

--- data/song/songController.js
+++ data/song/songController.js
@@ -1,17 +1,21 @@
 angular.module('myApp')
-    .controller('getSongsCtrl', function($scope, $http) {
+    .controller('getSongsCtrl', function($scope, $http, $timeout, $mdDialog) {
+        'use strict';
+        $scope.loadComplete = false;
         $http({
             url: 'http://192.168.95.173:8089/song/getall',
             method: 'GET'
         }).then(function successCallback(response) {
             $scope.songs = response.data;
+            $scope.loadComplete = true;
         }, function errorCallback(error) {
             console.log(ErrorEvent);
         });
         $scope.selected = [];
-        $scope.toggleLimitOptions = function() {
-            $scope.limitOptions = $scope.limitOptions ? undefined : [10, 20, 30];
-        };
+        $scope.limitOptions = [10, 20, 30];
+        // $scope.toggleLimitOptions = function() {
+        //     $scope.limitOptions = $scope.limitOptions ? undefined : [10, 20, 30];
+        // };
 
         $scope.options = {
             rowSelection: true,
@@ -25,7 +29,26 @@ angular.module('myApp')
         };
         $scope.query = {
             order: 'name',
-            limit: 10,
+            limit: 20,
             page: 1
+        };
+
+        $scope.OpenTab = function(song) {
+            window.open(song.link);
+        }
+        $scope.loadStuff = function() {
+            $scope.promise = $timeout(function() {
+                // loading
+            }, 2000);
+        }
+        $scope.addSong = function(ev) {
+            $mdDialog.show({
+                clickOutsideToClose: true,
+                controller: 'getSongsCtrl',
+                controllerAs: 'ctrl',
+                focusOnOpen: false,
+                targetEvent: event,
+                templateUrl: 'templates/add-song.html',
+            }).then($scope.getDesserts);
         };
     });
